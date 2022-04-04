9c2e4c224401d868c1784e99e1e299cc44906052
parthpandyappp <parthpandyappp@gmail.com>
2022-04-04 17:56:44 +0530

    [Edit]: class to className, cursor type to pointer

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,15 +4,15 @@ function Nav() {
   return (
     <nav>
       <Link to="/">
-        <h2 class="nav-title">
+        <h2 className="nav-title">
           <span>
             <img
-              class="logo"
+              className="logo"
               src="https://img.icons8.com/ios/50/000000/retro-tv.png"
               alt="logo"
             />
           </span>
-          /<span class="title-empha">Stre</span>Flix
+          /<span className="title-empha">Stre</span>Flix
         </h2>
       </Link>
 
@@ -21,10 +21,10 @@ function Nav() {
           type="search"
           name=""
           id="search-bar"
-          class="search-bar"
+          className="search-bar"
           placeholder="Your search goes here"
         />
-        <button class="btn btn-primary login-btn ml-2">Login</button>
+        <button className="btn btn-primary login-btn ml-2">Login</button>
       </div>
     </nav>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,79 +2,81 @@ import { Link } from "react-router-dom";
 
 function Home() {
   return (
-    <main class="center-hv">
-      <div class="container">
+    <main className="center-hv">
+      <div className="container">
         <div>
-          <h1 class="main-title">
+          <h1 className="main-title">
             <span>
               <img
-                class="logo"
+                className="logo"
                 src="https://img.icons8.com/ios/50/000000/retro-tv.png"
                 alt="logo"
               />
             </span>
-            /<span class="title-empha">Stre</span>Flix
+            /<span className="title-empha">Stre</span>Flix
           </h1>
-          <p class="main-slogan">//Stream at peace</p>
+          <p className="main-slogan">{"//"}Stream at peace</p>
           <p>
             Unlimited Movies, TV shows and more. Stream videos at peace, at your
             convenience. Drag your couch and start watching!!
           </p>
 
-          <button class="btn btn-primary">Explore Videos</button>
-          <button class="btn btn-secondary ml-2">
+          <Link to="/explore">
+            <button className="btn btn-primary">Explore Videos</button>
+          </Link>
+          <button className="btn btn-secondary ml-2">
             <a href="#category">Stream by Categories</a>
           </button>
         </div>
         <img
-          class="landing-img"
+          className="landing-img"
           src="https://assets.website-files.com/5d5e2ff58f10c53dcffd8683/5d5e304c8f10c501d7fd8a1d_meditating.svg"
-          alt="landing-image"
+          alt="landing"
         />
       </div>
 
-      <div class="pg-section">
-        <div class="section-title">
-          <h1 class="category-title" id="category">
+      <div className="pg-section">
+        <div className="section-title">
+          <h1 className="category-title" id="category">
             Categories
           </h1>
         </div>
-        <div class="category-container">
-          <div class="category">
-            <div class="categories-title">
+        <div className="category-container">
+          <div className="category">
+            <div className="categories-title">
               <h3>Education</h3>
             </div>
             <Link to="/explore">
               <img
-                class="category-img"
+                className="category-img"
                 src="https://i.ibb.co/K9Bx1SD/streflix-education.jpg"
-                alt=""
+                alt="cateogory-education"
               />
             </Link>
           </div>
 
-          <div class="category">
-            <div class="categories-title">
+          <div className="category">
+            <div className="categories-title">
               <h3>Gaming</h3>
             </div>
             <Link to="/explore">
               <img
-                class="category-img"
+                className="category-img"
                 src="https://i.ibb.co/GVgTxDZ/streflix-gaming.jpg"
-                alt=""
+                alt="cateogory-gaming"
               />
             </Link>
           </div>
 
-          <div class="category">
-            <div class="categories-title">
+          <div className="category">
+            <div className="categories-title">
               <h3>Art</h3>
             </div>
             <Link to="/explore">
               <img
-                class="category-img"
+                className="category-img"
                 src="https://i.ibb.co/pKtQdQT/streflix-art.jpg"
-                alt=""
+                alt="cateogory-art"
               />
             </Link>
           </div>
