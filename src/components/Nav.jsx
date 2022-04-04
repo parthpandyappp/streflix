1bf336f42e0a35a18b6f1ffc980498826511d1e8
parthpandyappp <parthpandyappp@gmail.com>
2022-04-04 17:33:13 +0530

    [Feat]: Homepage design

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,29 @@
 function Nav() {
-  return <h1>Nav</h1>;
+  return (
+    <nav>
+      <h2 class="nav-title">
+        <span>
+          <img
+            class="logo"
+            src="https://img.icons8.com/ios/50/000000/retro-tv.png"
+            alt="logo"
+          />
+        </span>
+        /<span class="title-empha">Stre</span>Flix
+      </h2>
+
+      <div>
+        <input
+          type="search"
+          name=""
+          id="search-bar"
+          class="search-bar"
+          placeholder="Your search goes here"
+        />
+        <button class="btn btn-primary login-btn ml-2">Login</button>
+      </div>
+    </nav>
+  );
 }
 
 export { Nav };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,85 @@
 function Home() {
-  return <h1>Home</h1>;
+  return (
+    <main class="center-hv">
+      <div class="container">
+        <div>
+          <h1 class="main-title">
+            <span>
+              <img
+                class="logo"
+                src="https://img.icons8.com/ios/50/000000/retro-tv.png"
+                alt="logo"
+              />
+            </span>
+            /<span class="title-empha">Stre</span>Flix
+          </h1>
+          <p class="main-slogan">//Stream at peace</p>
+          <p>
+            Unlimited Movies, TV shows and more. Stream videos at peace, at your
+            convenience. Drag your couch and start watching!!
+          </p>
+
+          <button class="btn btn-primary">Explore Videos</button>
+          <button class="btn btn-secondary ml-2">
+            <a href="#category">Stream by Categories</a>
+          </button>
+        </div>
+        <img
+          class="landing-img"
+          src="https://assets.website-files.com/5d5e2ff58f10c53dcffd8683/5d5e304c8f10c501d7fd8a1d_meditating.svg"
+          alt="landing-image"
+        />
+      </div>
+
+      <div class="pg-section">
+        <div class="section-title">
+          <h1 class="category-title" id="category">
+            Categories
+          </h1>
+        </div>
+        <div class="category-container">
+          <div class="category">
+            <div class="categories-title">
+              <h3>Education</h3>
+            </div>
+            <a href="./pages/markup/prod-list.html">
+              <img
+                class="category-img"
+                src="https://i.ibb.co/K9Bx1SD/streflix-education.jpg"
+                alt=""
+              />
+            </a>
+          </div>
+
+          <div class="category">
+            <div class="categories-title">
+              <h3>Gaming</h3>
+            </div>
+            <a href="./pages/markup/prod-list.html">
+              <img
+                class="category-img"
+                src="https://i.ibb.co/GVgTxDZ/streflix-gaming.jpg"
+                alt=""
+              />
+            </a>
+          </div>
+
+          <div class="category">
+            <div class="categories-title">
+              <h3>Art</h3>
+            </div>
+            <a href="./pages/markup/prod-list.html">
+              <img
+                class="category-img"
+                src="https://i.ibb.co/pKtQdQT/streflix-art.jpg"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export { Home };
